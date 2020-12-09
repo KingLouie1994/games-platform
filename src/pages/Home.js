518651fdeff068a690abb73542b6980add3adbf9
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-09 12:37:41 +0100

    home page renders a basic styled card for every upcoming game fetched from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-// Imports from react
+// Imports from React
 import { useEffect } from "react";
 
-// Imports from redux
-import { useDispatch } from "react-redux";
+// Imports from Redux
+import { useDispatch, useSelector } from "react-redux";
 
 // Import Actions
 import { loadGames } from "../actions/gamesAction";
+
+// Import Components
+import Game from "../components/Game";
+
+// Imports for Styling
+import styled from "styled-components";
+
+// Imports for Animation
+import { motion } from "framer-motion";
 
 const Home = () => {
   // Fetch Games
@@ -13,13 +22,47 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  // Make Games Data usable
+  const { popularGames, newGames, upcomingGames } = useSelector(
+    (state) => state.games
+  );
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcomingGames.map((game) => {
+          return (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          );
+        })}
+      </Games>
+    </GameList>
   );
 };
+
+// Styled Components
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
